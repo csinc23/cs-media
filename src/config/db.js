294506bf197ac479f34106ddaf8e47f4eea334ef
shoussame <hsouhane@gmail.com>
2023-01-09 19:41:26 +0100

    Credentials

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb+srv://cosadmininc:mPvxH6aMAR2dBjex@cluster0.omsh1c8.mongodb.net/media?retryWrites=true",
+  url: "mongodb+srv://cosuseracc:AUbBxdO3STgd4GYv@cluster0.omsh1c8.mongodb.net/media?retryWrites=true",
   database: "media",
   imgBucket: "photos",
 };
